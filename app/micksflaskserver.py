de927625bbe08dd84882ac4b1f8e1986135597a5
Mukund Chandra <mukundchandra15@gmail.com>
2022-05-24 13:09:33 +0530

    Added Post Request Support

--- app/micksflaskserver.py
+++ app/micksflaskserver.py
@@ -15,6 +15,20 @@ def sendback_json():
 	json_obj = json.dumps({"FirstName":"Mukund","LastName":"Chandra","Text":"I welcome you to this place"})
 	return json_obj
 
+@app.route('/submitrequest/')
+def sample_form1():
+	return flask.render_template('testinputpage.html')
+
+@app.route('/postrequesttest/',methods=['POST'])
+def get_posted_data():
+	if flask.request.method == 'POST':
+		check1 = flask.request.form.get('CheckboxTest1')
+		text1 = flask.request.form.get('TextboxTest1')
+		print(check1,text1)
+		json_obj = json.dumps({"Checkbox":check1,"Text":text1})
+		return json_obj
+
+
 if __name__ == "__main__":
 	# app.run() starts the Flask app, you need to call this function to start it.
 	# It will most likely by running on http://127.0.0.1:5000/
